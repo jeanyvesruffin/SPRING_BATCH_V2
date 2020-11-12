dab158a9b2cad5dab506cd114af5b95c1596beb4
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-11-12 11:06:31 +0100

    first commit

---
+++ src/test/java/com/ruffin/SPRING_BATCH_V2/SpringBatchV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffin.SPRING_BATCH_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
